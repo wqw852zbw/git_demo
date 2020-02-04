0f4a735100fe90d223f78d55242634c9fbcccbed
zbw123aini <1595320647@qq.com>
2020-02-04 17:04:17 +0800

    [1.0][第一次账号提交]

---
+++ src/main/java/com/qf/Main.java
@@ -0,0 +1,12 @@
+package com.qf;
+
+/**
+ * @version 1.0
+ * @users EX-ZHENGBOWEN001
+ * @date 2020/2/4 16:52
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello git");
+    }
+}
